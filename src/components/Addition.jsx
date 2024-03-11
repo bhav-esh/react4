3844b6cd61484660a2d4f7ed9a9c54ac7f06128a
bhav-esh <97869586+bhav-esh@users.noreply.github.com>
2024-03-11 10:46:44 +0530

    Add files via upload

---
+++ src/components/Addition.jsx
@@ -0,0 +1,22 @@
+function Addition(){
+
+    function updateX(e){
+        const target = e.target;
+        const value = target.value;
+        
+    }
+
+    return(
+        <div>
+            <h1>My Addition</h1>
+            <input/>
+            <input/>
+            <button>
+            Submit
+            </button>
+            <pre>
+                Result is
+            </pre>
+        </div>
+    )
+}
---
+++ src/components/Subtraction.jsx
@@ -0,0 +1,16 @@
+function Subtraction(){
+
+    return(
+        <div>
+            <h1>My Subtraction</h1>
+            <input/>
+            <input/>
+            <button>
+            Submit
+            </button>
+            <pre>
+                Result is
+            </pre>
+        </div>
+    )
+}
